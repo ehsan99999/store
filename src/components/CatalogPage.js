f1622f96eb960d3100aac18f89627d240167b1c5
ehsan <ehsan.set1@gmail.com>
2019-06-15 12:18:49 -0700

    collapsible navbar add

--- src/components/CatalogPage.js
+++ src/components/CatalogPage.js
@@ -5,10 +5,7 @@ import  ProductCard  from "./ProductCard";
 import  SiteHeader  from "./SiteHeader";
 import  Foooter  from "./Foooter";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch,faDollarSign, faStarHalfAlt,faSortAmountDown} from '@fortawesome/free-solid-svg-icons'
-import dollarSymbol from '../img/dollarSymbol.svg'
-import upArrow from '../img/upArrow.svg'
-import downArrow from '../img/downArrow.svg'
+import { faSearch,faDollarSign, faStarHalfAlt,faSortAmountDown,faCog} from '@fortawesome/free-solid-svg-icons'
 import dollarSignDown from '../img/dollarSignDown.png'
 import dollarSignUp from '../img/dollarSignUp.png'
 
@@ -117,7 +114,6 @@ class CatalogPage extends Component {
         /*TODO add search
             - add Timer            
         */
-        //TODO add sort {price up - price down - rating }
 
         console.log(this.props)
         if(this.props.categories.length === 0 ){
@@ -145,9 +141,9 @@ class CatalogPage extends Component {
                     className=" list-group-item list-group-item-action d-flex" 
                     key={"categoryId-"+category.id} 
                     onClick={()=>{this.props.setSelectedCategory(category.id,this.props.loadedProductsOffset,this.props.sortBy)}}>
-                        <img src={category.image} alt={"category of "+category.name} className="mr-2" />
+                        <img src={category.image} alt={"category of "+category.name} className="d-none d-md-block mr-2" />
                         <span className={"font-weight-bold my-auto "+active}> {category.name} </span>
-                        <span className="badge badge-pill badge-info font-weight-light  my-auto pull-right ml-auto">{category.numberOfProducts}</span>
+                        <span className="badge badge-pill badge-info font-weight-light  my-auto pull-right ml-auto d-block d-md-none d-lg-block">{category.numberOfProducts}</span>
                 </li>
             )
         });
@@ -170,34 +166,60 @@ class CatalogPage extends Component {
             <div className="catalogPage">
                 <SiteHeader />
                 <div className="headerMargin  w-100  ">
-                    <nav className="bg-light  ">
-                        <div className="d-flex flex-row-reverse flex-row ">
-                            <form className="" onSubmit={this.formPreventDefault}>
-                                <div class="searchbar bg-info mx-1">
-                                    <input class="search_input text-light "
-                                            type="text"
-                                            name="" 
-                                            value={this.state.searchKey} 
-                                            placeholder="Search..." 
-                                            onChange={this.changeSearchKey} />
+                <nav className="navbar navbar-expand-md navbar-light bg-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className=""><FontAwesomeIcon icon={faCog} className="" /></span>
+                    </button>
 
-                                    <button type="submit" class="search_icon" onClick={this.search}><FontAwesomeIcon icon={faSearch} className="" /></button>
-                                </div>
-                            </form>
-                            <SortComponent  setSortMethod={this.setSortMethod} sortBy={this.props.sortBy}  />
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item p-0  d-none d-md-block">
+                                <form className="" onSubmit={this.formPreventDefault}>
+                                        <div className="searchbar bg-info mx-1">
+                                            <input className="search_input text-light "
+                                                    type="text"
+                                                    name="" 
+                                                    value={this.state.searchKey} 
+                                                    placeholder="Search..." 
+                                                    onChange={this.changeSearchKey} />
 
-                        </div>
+                                            <button type="submit" className="search_icon" onClick={this.search}><FontAwesomeIcon icon={faSearch} className="" /></button>
+                                        </div>
+                                </form>
+                            </li>
+                            <li className="nav-item ">
+                                <SortComponentSmall  setSortMethod={this.setSortMethod} sortBy={this.props.sortBy}  />
+                            </li>
 
-                    </nav>
+                            <li className="nav-item d-block d-md-none">
+                                <form className="form-inline  input-group my-2 my-lg-0">
+                                <input className="form-control btn-outline-info mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-info input-group-append " type="submit"><FontAwesomeIcon icon={faSearch} className="" /></button>
+                                </form>
+                            </li>
+                            <li className="nav-item dropdown d-block d-md-none">
+                                <a className="btn btn-outline-info text-info w-100  nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Categories
+                                </a>
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        {categoriesJsx}
+                                    </ul>
+
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+
                 </div>{/* Div Navbar  */}
                 <div className="row  mx-0 " >
-                    <div className="col-3 ">
-                        <ul className=" categoriesList list-group list-group-flush  ">
+                    <div className="col-md-3 d-none d-md-block">
+                        <ul className="categoriesList list-group list-group-flush  ">
                             {categoriesJsx}
 
                         </ul>
                     </div>
-                    <div className="col-9">
+                    <div className="col-md-9">
                         <Products  products = {this.props.products} title={activeCategoryName}  ></Products>
                         {showMoreButtonJsx}
 
@@ -213,13 +235,57 @@ class CatalogPage extends Component {
     }
 }
 
+function SortComponentSmall({setSortMethod,sortBy}){
+    let sortByJsx;
+    switch (sortBy) {
+        case "priceLowToHigh":
+                sortByJsx =  (<span className="align-middle">
+                            Price: Low to High 
+                        </span>);
+                break;
+        case "priceHighToLow":
+                sortByJsx = (<span className="align-middle">
+                    Price: High To Low
+                </span>);
+                break;
+        case "ratingHighToLow":
+                sortByJsx = (<span className="align-middle">
+                    Rating: High To Low
+                </span>)
+                break;
+
+        default:
+                sortByJsx = (<span className="align-middle">
+                    Sort Products
+                </span>)
+                break;
+
+    }
+    
+    return(
+        <div class="dropdown">
+            <button class="btn btn-outline-info dropdown-toggle w-100" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {sortByJsx}
+            </button>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item text-info"  onClick={()=>{setSortMethod("priceLowToHigh")}}>Price: Low to High</a>
+                <a class="dropdown-item text-info"  onClick={()=>{setSortMethod("priceHighToLow")}}>Price: High To Low</a>
+                <a class="dropdown-item text-info"  onClick={()=>{setSortMethod("ratingHighToLow")}}>Price: Rating</a>
+                <a class="dropdown-item text-info"  onClick={()=>{setSortMethod("")}}>Default</a>
+            </div>
+        </div>
+
+
+    )
+}
+
 function SortComponent({setSortMethod,sortBy}){
     let sortByJsx;
      
     switch (sortBy) {
         case "priceLowToHigh":
                 sortByJsx =  (<span className="align-middle">
-                            <img src={dollarSignUp} width={25} height={25}  />
+                            <img src={dollarSignUp} width={25} height={25}  /> 
                         </span>);
                 break;
         case "priceHighToLow":
@@ -239,11 +305,11 @@ function SortComponent({setSortMethod,sortBy}){
     
     return(
         <div className="btn-group">
-            <button type="button" className="btn btn-sm btn-info dropdown-toggle align-middle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i type="button" className="btn btn-sm btn-info dropdown-toggle align-middle " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {/* <FontAwesomeIcon icon={faSortAmountDown} className="" /> */}
                 {/* <img src={upArrow} width={20} height={20} fill-color="#ffffff" /> */}
                 {sortByJsx}
-            </button>
+            </i>
             <div className="dropdown-menu ">
                 <a className="dropdown-item text-info" onClick={()=>{setSortMethod("priceLowToHigh")}}>
                     <FontAwesomeIcon icon={faDollarSign} className="mr-2" /> Low to High
